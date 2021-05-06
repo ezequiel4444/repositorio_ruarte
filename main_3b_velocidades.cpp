1fb99e2ee5878f882e02e8e15a147d19990df789
ezequiel4444 <83045917+ezequiel4444@users.noreply.github.com>
2021-05-06 18:43:35 -0300

    Add files via upload

---
+++ main_3b_velocidades.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   double v;
+   cout << "Ingrese v en unidades de [m/s] =" << endl;
+   cin >> v ;
+   cout << " Informar el valor de v en [km/h] =" << v * 3.6 << endl;
+   cout << "El programa ha finalizado." << endl;
+
+    return 0;
+}
